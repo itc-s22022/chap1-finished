bcdde1027d32722450ed4fdf4696833efb78af16
itc-s22022 <s22022@std.it-college.ac.jp>
2023-02-19 11:50:51 +0000

    9.1

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -8,8 +8,12 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-colum
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Schedule({
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Schedule ({
   title,
   publish,
   content,
@@ -26,10 +30,8 @@ export default function Schedule({
         pageImgW={eyecatch.width}
         pageImgH={eyecatch.height}
       />
-
       <article>
         <PostHeader title={title} subtitle="Blog Article" publish={publish} />
-        
         <figure>
           <Image
             src={eyecatch.url}
@@ -39,9 +41,10 @@ export default function Schedule({
             height={eyecatch.height}
             sizes="(min-width: 1152px) 1152px, 100vw"
             priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
-
         <TwoColumn>
           <TwoColumnMain>
             <PostBody>
@@ -52,25 +55,29 @@ export default function Schedule({
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
-
       </article>
     </Container>
   )
 }
 
-export async function getStaticProps () {
-  const slug = 'schedule'
+export async function getStaticProps() {
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description,
     },
